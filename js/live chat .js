20fb6cf39d06d7a85223b477f8aa8af471e052fa
multidimensi <ahmadsyauqiarisandi27@gmail.com>
2023-02-15 10:24:12 +0700

    update foto final

---
+++ js/live chat .js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = '8c7315d8-c8c6-4f69-a2e4-34957d3bf094';
+(function () {
+  d = document;
+  s = d.createElement('script');
+  s.src = 'https://client.crisp.chat/l.js';
+  s.async = 1;
+  d.getElementsByTagName('head')[0].appendChild(s);
+})();
